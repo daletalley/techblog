86b644d9894db93d862f286d2fb55ffaad144279
daletalley <dtalley32@icloud.com>
2023-12-01 19:08:33 -0600

    fixingg posts

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+// Import the homeRoutes module
 const homeRoutes = require('./homeRoutes');
 
+// Import the api module
+const apiRoutes = require('./api');
+
+// Use the homeRoutes module for requests to the root URL ('/')
 router.use('/', homeRoutes);
+
+// Use the apiRoutes module for requests to the '/api' URL
 router.use('/api', apiRoutes);
 
+// Export the router object to make it available for other files
 module.exports = router;
